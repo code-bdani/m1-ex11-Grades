fbc2f66299ca6829d1c46464c3e3ed1c65901bee
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-04 14:22:02 -0600

    trying it on my own

--- grades.js
+++ grades.js
@@ -1,65 +1,77 @@
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
-const grades = {a:0, b:0, c:0, d:0, f:0}
+const grades = {} // You'll need to change this line of code
 
 for (let i = 0; i < scores.length; i++) {
-    switch(true) {
-        case (scores[i] >= 91 && scores[i] <= 100):
-            grades.a += 1;
-            console.log("in for", scores[i]);
-            break;
+    /*
+      If the score is greater than 90, increment grades.A by 1.
 
-        case (scores[i] >= 81 && scores[i] < 91):
-            grades.b += 1;
-            console.log("in for", scores[i]);
-            break;
+      You can use a series of `if/then` blocks, but you could
+      also achieve this with a switch statement.
+    */
+}
+
+// const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60]
+// const grades = {a:0, b:0, c:0, d:0, f:0}
+
+// for (let i = 0; i < scores.length; i++) {
+//     switch(true) {
+//         case (scores[i] >= 91 && scores[i] <= 100):
+//             grades.a += 1;
+//             console.log("in for", scores[i]);
+//             break;
+
+//         case (scores[i] >= 81 && scores[i] < 91):
+//             grades.b += 1;
+//             console.log("in for", scores[i]);
+//             break;
         
-        case (scores[i] >= 71 && scores[i] < 81):
-            grades.c += 1;
-            console.log("in for", scores[i]);
-            break; 
+//         case (scores[i] >= 71 && scores[i] < 81):
+//             grades.c += 1;
+//             console.log("in for", scores[i]);
+//             break; 
             
-        case (scores[i] >= 61 && scores[i] < 71):
-            grades.d += 1;
-            console.log("in for", scores[i]);
-            break;
+//         case (scores[i] >= 61 && scores[i] < 71):
+//             grades.d += 1;
+//             console.log("in for", scores[i]);
+//             break;
             
-        case (scores[i] < 61): 
-            grades.f += 1;
-            console.log("in for", scores[i]);
-            break;
+//         case (scores[i] < 61): 
+//             grades.f += 1;
+//             console.log("in for", scores[i]);
+//             break;
     
-        default:
-            console.log("ERROR!");
-    }
-}
+//         default:
+//             console.log("ERROR!");
+//     }
+// }
 
-var lowHigh = scores.sort(function(a, b){return a - b});
+// var lowHigh = scores.sort(function(a, b){return a - b});
 
-console.log("lowest",lowHigh[0]);
-console.log("highest",lowHigh[lowHigh.length - 1]);
+// console.log("lowest",lowHigh[0]);
+// console.log("highest",lowHigh[lowHigh.length - 1]);
 
-var currentGradeCount = 0;
-var mostFrequent = []; 
+// var currentGradeCount = 0;
+// var mostFrequent = []; 
 
-var newGradeCount = 100;
-var leastFrequent = [];
-for (var x in grades) {
-    if (grades[x] > currentGradeCount) {
-        mostFrequent = [x];
-        currentGradeCount = grades[x];
-    } else if (grades[x] === currentGradeCount){
-        mostFrequent.push(x);
-    } 
+// var newGradeCount = 100;
+// var leastFrequent = [];
+// for (var x in grades) {
+//     if (grades[x] > currentGradeCount) {
+//         mostFrequent = [x];
+//         currentGradeCount = grades[x];
+//     } else if (grades[x] === currentGradeCount){
+//         mostFrequent.push(x);
+//     } 
     
-    if (grades[x] < newGradeCount) {
-        leastFrequent = [x];
-        newGradeCount = grades[x];
-        } else if (grades[x] === newGradeCount) {
-            leastFrequent.push(x);
-        }
+//     if (grades[x] < newGradeCount) {
+//         leastFrequent = [x];
+//         newGradeCount = grades[x];
+//         } else if (grades[x] === newGradeCount) {
+//             leastFrequent.push(x);
+//         }
 
-}
-console.log("least frequent", leastFrequent);
-console.log("most frequent", mostFrequent);
-console.log(lowHigh);
-console.log(grades);
+// }
+// console.log("least frequent", leastFrequent);
+// console.log("most frequent", mostFrequent);
+// console.log(lowHigh);
+// console.log(grades);
